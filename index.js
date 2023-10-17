3418ac152b01d75ade02f829b4ef0228f3b5426a
rumbo <rumbo.9370@gmail.com>
2023-10-17 13:54:46 +0200

    removed encoded and dead code

--- index.js
+++ index.js
@@ -9,21 +9,20 @@ const server = http.createServer((req, res) => {
     switch(req.url) {
         case "/":
             res.writeHead(200, {'Content-Type': 'text/html'});
-            let template = fs.readFileSync('design1/index.html', {encoding: "utf-8"})
+            let template = fs.readFileSync('design1/index.html', "utf-8")
             const data = JSON.parse(fs.readFileSync('posts.json', 'utf8'));
             const html = ejs.render(template, { title: 'John', articles: data });
             res.write(html);
-            //res.write();
             res.end();
             break;
         case "/styles/styles.css":
             res.writeHead(200, {'Content-Type': 'text/css'});
-            res.write(fs.readFileSync('design1/styles/styles.css', {encoding: "utf-8"}));
+            res.write(fs.readFileSync('design1/styles/styles.css', "utf-8"));
             res.end();
             break;
         case "/scripts/main.js":
             res.writeHead(200, {'Content-Type': 'text/javascript'});
-            res.write(fs.readFileSync('design1/scripts/main.js', {encoding: "utf-8"}));
+            res.write(fs.readFileSync('design1/scripts/main.js', "utf-8"));
             res.end();
             break;
         default:

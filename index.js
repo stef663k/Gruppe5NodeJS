15b3fc6573d9571a11990c2541a76889e155d1f9
rumbo <rumbo.9370@gmail.com>
2023-10-17 12:07:48 +0200

    server get files
    the server can get the html, css and js files

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
 const http = require('http');
-
-const hostname = "localhost";
+const fs = require('fs');
 const port = 3000;
 
+
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World");
+/* fs.readFile('design1/index.html', {encoding: "utf-8"},  function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+}); */
+
+    /* if(req.url === "/") {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(fs.readFileSync('design1/index.html', {encoding: "utf-8"}));
+        res.end();
+    } 
+    if(req.url === "/styles/styles.css") {
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.write(fs.readFileSync('design1/styles/styles.css', {encoding: "utf-8"}));
+        res.end();
+    }
+    
+    else{
+        res.end("Invalid Request")
+    } */
+
+    switch(req.url) {
+        case "/":
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(fs.readFileSync('design1/index.html', {encoding: "utf-8"}));
+            res.end();
+            break;
+        case "/styles/styles.css":
+            res.writeHead(200, {'Content-Type': 'text/css'});
+            res.write(fs.readFileSync('design1/styles/styles.css', {encoding: "utf-8"}));
+            res.end();
+            break;
+        case "/scripts/main.js":
+            res.writeHead(200, {'Content-Type': 'text/javascript'});
+            res.write(fs.readFileSync('design1/scripts/main.js', {encoding: "utf-8"}));
+            res.end();
+            break;
+        default:
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write('error'); //read the file & write the data content
+      res.end();
+      break;
+
+
+    }
+
+
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
